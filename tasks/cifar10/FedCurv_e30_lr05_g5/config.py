c12b89782ea03a20517c027ec792e2554d0b5a16
Ada <xucc19982@gmail.com>
2022-02-06 11:09:21 +0800

    add tasks/cifar10

---
+++ tasks/cifar10/FedCurv_e30_lr05_g5/config.py
@@ -0,0 +1,44 @@
+from FedUtils.models.cifar10.resnet9 import Model
+import torch
+from functools import partial
+from FedUtils.fed.fedcurv import FedCurv
+from torchvision import transforms
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+# Normalize test set same as training set without augmentation
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+config = {
+
+    "seed": 1,
+    "model": Model,
+    "inner_opt": partial(torch.optim.SGD, lr=5e-2, weight_decay=0),
+    "optimizer": FedCurv,
+    "model_param": (10,),
+    "inp_size": (3*32*32,),
+    "train_path": "data/cifar-10-batches-py/data_uni/train/",
+    "test_path": ["data/cifar-10-batches-py/data_uni/valid/", "data/cifar-10-batches-py/data_uni/test/"],
+    "clients_per_round": 100,
+    "num_rounds": 240,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 30,
+    "batch_size": 5,
+    "use_fed": 1,
+    "log_path": "tasks/cifar10/FedCurv_e30_lr05_g5/train.log",
+    "train_transform": transform_train,
+    "test_transform": transform_test,
+    "eval_train": False,
+    "gamma": 1e-5
+
+
+}
---
+++ tasks/cifar10/FedReg_mask/config.py
@@ -0,0 +1,47 @@
+from FedUtils.models.cifar10.resnet9 import Model
+from functools import partial
+from FedUtils.fed.fedreg import FedReg
+from torch.optim import SGD
+from torchvision import transforms
+
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+# Normalize test set same as training set without augmentation
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+config = {
+
+    "seed": 1,
+    "model": Model,
+    "inner_opt": partial(SGD, lr=1.0e-1),
+    "optimizer": FedReg,
+    "model_param": (10,),
+    "inp_size": (3*32*32,),
+    "train_path": "/data/xcc/FedReg_data/data/cifar-10-batches-py/data_uni/train/",
+    "test_path": ["/data/xcc/FedReg_data/data/cifar-10-batches-py/data_uni/valid/", "/data/xcc/FedReg_data/data/cifar-10-batches-py/data_uni/test/"],
+    "clients_per_round": 100,
+    "num_rounds": 240,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 30,
+    "batch_size": 5,
+    "use_fed": 1,
+    "log_path": "tasks/cifar10/FedReg_mask/train.log",
+    "train_transform": transform_train,
+    "test_transform": transform_test,
+    "eval_train": False,
+    "gamma": 0.5,
+    "eta_s": -1e-3,
+    "add_mask": 1
+
+
+}
