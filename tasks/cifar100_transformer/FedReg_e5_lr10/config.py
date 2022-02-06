72beee1521e77ab91e1982be961a0153243c15d2
Ada <xucc19982@gmail.com>
2022-02-06 13:24:53 +0800

    add cifar100_transformer

---
+++ tasks/cifar100_transformer/FedReg_e5_lr10/config.py
@@ -0,0 +1,48 @@
+from FedUtils.models.transformer.model import Model
+import torch
+from functools import partial
+from FedUtils.fed.fedreg import FedReg
+from torchvision import transforms, utils
+
+transform_train = transforms.Compose([
+    transforms.Resize(512),
+    transforms.RandomCrop(384),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=0.5, std=0.5)
+])
+
+# Normalize test set same as training set without augmentation
+transform_test = transforms.Compose([
+    transforms.Resize(384),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=0.5, std=0.5)
+])
+
+
+config = {
+
+    "seed": 1,
+    "model": Model,
+    "inner_opt": partial(torch.optim.SGD, lr=10e-2),
+    "optimizer": FedReg,
+    "model_param": (10,),
+    "inp_size": (3*32*32,),
+    "train_path": "data/cifar-100-python/data/train/",
+    "test_path": ["data/cifar-100-python/data/valid/", "data/cifar-100-python/data/test/"],
+    "clients_per_round": 500,
+    "num_rounds": 100,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 5,
+    "batch_size": 5,
+    "use_fed": 1,
+    "log_path": "tasks/cifar100_transformer/FedReg_e5_lr10/train.log",
+    "train_transform": transform_train,
+    "test_transform": transform_test,
+    "eval_train": False,
+    "gamma": 0.02,  # gamma_func,
+    "eta_s": -1e-2
+
+
+}
---
+++ tasks/cifar100_transformer/SGD/config.py
@@ -0,0 +1,45 @@
+from FedUtils.models.transformer.model import Model
+import torch
+from functools import partial
+from FedUtils.fed.fedavg_sgd import FedAvg
+from torchvision import transforms, utils
+
+transform_train = transforms.Compose([
+    transforms.Resize(512),
+    transforms.RandomCrop(384),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=.5, std=.5)
+])
+
+# Normalize test set same as training set without augmentation
+transform_test = transforms.Compose([
+    transforms.Resize(384),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=.5, std=.5)
+])
+
+config = {
+
+    "seed": 1,
+    "model": Model,
+    "inner_opt": partial(torch.optim.SGD, lr=10e-2, weight_decay=0),
+    "optimizer": FedAvg,
+    "model_param": (10,),
+    "inp_size": (3*384*384,),
+    "train_path": "data/cifar-100-python/data/train/",
+    "test_path": ["data/cifar-100-python/data/valid/", "data/cifar-100-python/data/test/"],
+    "clients_per_round": 500,
+    "num_rounds": 100,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 1,
+    "batch_size": 10,
+    "use_fed": 1,
+    "log_path": "tasks/cifar100_transformer/SGD_lr10/train.log",
+    "train_transform": transform_train,
+    "test_transform": transform_test,
+    "eval_train": False,
+
+
+}
