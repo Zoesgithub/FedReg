d52288581a89528ce2f0ba2359f2b334ccbc9d43
zoesgithub <xcc14@mails.tsinghua.edu.cn>
2022-07-03 14:29:48 +0800

    add mnist task in fedprox

---
+++ tasks/mnist_fedprox/FedAvg/config.py
@@ -0,0 +1,31 @@
+from FedUtils.models.mnist.cnn import Model
+import torch
+from functools import partial
+from FedUtils.fed.fedavg import FedAvg
+
+config = {
+
+    "seed": 1,
+    "model": partial(Model, learning_rate=1e-1),
+    "inner_opt": None,
+    "optimizer": FedAvg,
+    "model_param": (10,),
+    "inp_size": (784,),
+    "train_path": "data/mnist/data/train/",
+    "test_path": ["data/mnist/data/valid/", "data/mnist/data/test/"],
+    "clients_per_round": 10,
+    "num_rounds": 200,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 20,
+    "batch_size": 10,
+    "use_fed": 1,
+    "log_path": "tasks/mnist_fedprox/FedAvg/train.log",
+
+    "train_transform": None,
+    "test_transform": None,
+    "eval_train": True,
+
+
+
+}
---
+++ tasks/mnist_fedprox/FedReg/config.py
@@ -0,0 +1,28 @@
+from FedUtils.models.mnist.cnn import Model
+from functools import partial
+from FedUtils.fed.fedreg import FedReg
+from torch.optim import SGD
+config = {
+
+    "seed": 1,
+    "model": partial(Model, learning_rate=1e-1, p_iters=10, ps_eta=2e-1, pt_eta=2e-3),
+    "inner_opt": None,
+    "optimizer": FedReg,
+    "model_param": (10,),
+    "inp_size": (784,),
+    "train_path": "data/mnist/data/train/",
+    "test_path": ["data/mnist/data/valid/", "data/mnist/data/test/"],
+    "clients_per_round": 10,
+    "num_rounds": 200,
+    "eval_every": 1,
+    "drop_percent": 0.0,
+    "num_epochs": 20,
+    "batch_size": 10,
+    "use_fed": 1,
+    "log_path": "tasks/mnist_fedprox/FedReg/train.log",
+    "train_transform": None,
+    "test_transform": None,
+    "eval_train": True,
+    "gamma": 0.5,
+
+}
